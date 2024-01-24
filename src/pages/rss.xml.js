dae94916284abfac9b4ae576a97472492137f5a4
Sebastián Salazar <ensuramerica@gmail.com>
2024-01-24 08:37:43 -0500

    crm

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,27 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = await getCollection('development');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.slug}/`,
+			link: `/programacion/${post.slug}/`,
+		})),
+	});
+}
+
+export async function GET(context) {
+	const posts = await getCollection('marketing');
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: context.site,
+		items: posts.map((post) => ({
+			...post.data,
+			link: `/marketing/${post.slug}/`,
 		})),
 	});
 }
