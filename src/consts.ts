9944fe90cae2c5be0d07ef1d2660d545529cffbc
Sebastián Salazar <ensuramerica@gmail.com>
2024-01-24 08:37:43 -0500

    crm

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Programación Web';
-export const SITE_DESCRIPTION = 'Un blog sobre programación web';
+export const SITE_TITLE = 'Shan Lat';
+export const SITE_DESCRIPTION = 'A blog to write about different topics.';
 export const BASE_URL = '';
