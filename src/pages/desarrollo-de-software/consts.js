dbafba4c97f153a42e298442b434ee071b1166d3
Sebastián Salazar <ensuramerica@gmail.com>
2024-01-26 11:11:05 -0500

    desarrollo de software

---
+++ src/pages/desarrollo-de-software/consts.js
@@ -0,0 +1,5 @@
+const shortTitle = 'Desarrollo de Software';
+const title = `Aprende ${shortTitle}: Artículos y Guías en Nuestro Blog | Shan.lat`;
+const description = 'Explora nuestro blog de desarrollo de software para obtener consejos, tutoriales y recursos que te ayudarán a dominar el arte del desarrollo de aplicaciones y programas.';
+
+export { shortTitle, title, description }
