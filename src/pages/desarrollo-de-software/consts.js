28c4afc3c140262b24788517fed803a1c5d2703f
Sebastián Salazar <ensuramerica@gmail.com>
2024-01-26 20:01:05 -0500

    Marketing Digital

--- src/pages/desarrollo-de-software/consts.js
+++ src/pages/desarrollo-de-software/consts.js
@@ -1,5 +1,17 @@
-const shortTitle = 'Desarrollo de Software';
-const title = `Aprende ${shortTitle}: Artículos y Guías en Nuestro Blog | Shan.lat`;
+
+const blogUrl = '/desarrollo-de-software';
 const description = 'Explora nuestro blog de desarrollo de software para obtener consejos, tutoriales y recursos que te ayudarán a dominar el arte del desarrollo de aplicaciones y programas.';
 
-export { shortTitle, title, description }
+const eyebrow = 'Aprende';
+const shortTitle = 'Desarrollo de Software';
+const below = 'Artículos y Guías en Nuestro Blog | Shan.lat'
+const title = `${eyebrow} ${shortTitle}: ${below}`;
+
+export { 
+  below, 
+  blogUrl,
+  description,
+  eyebrow,
+  shortTitle,
+  title,
+}
---
+++ src/pages/marketing-digital/consts.js
@@ -0,0 +1,13 @@
+const blogUrl = '/marketing-digital';
+const description = 'Descubre las últimas tendencias, estrategias efectivas y consejos prácticos en nuestro blog sobre marketing digital. Obtén insights sobre cómo mejorar tu presencia online, mejorar la fidelidad de tus clientes y alcanzar tus objetivos comerciales. ¡Explora ahora!';
+const shortTitle = 'Marketing Digital';
+const below = 'Estrategias, Tendencias y Consejos | Shan.lat';
+const title = `${shortTitle}: ${below}`;
+
+export {
+  below,
+  blogUrl,
+  description,
+  shortTitle,
+  title,
+}
