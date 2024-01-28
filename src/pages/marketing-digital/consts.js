ecf57ca1adf88c68d516e9562fa332a8ae6c27ab
Sebastián Salazar <ensuramerica@gmail.com>
2024-01-27 20:06:26 -0500

    Titles

--- src/pages/marketing-digital/consts.js
+++ src/pages/marketing-digital/consts.js
@@ -1,13 +1,12 @@
-const blogUrl = '/marketing-digital';
+const blogPath = '/marketing-digital';
 const description = 'Descubre las últimas tendencias, estrategias efectivas y consejos prácticos en nuestro blog sobre marketing digital. Obtén insights sobre cómo mejorar tu presencia online, mejorar la fidelidad de tus clientes y alcanzar tus objetivos comerciales. ¡Explora ahora!';
-const shortTitle = 'Marketing Digital';
+const blogKeyword = 'Marketing Digital';
 const below = 'Estrategias, Tendencias y Consejos | Shan.lat';
-const title = `${shortTitle}: ${below}`;
 
 export {
   below,
-  blogUrl,
+  blogPath,
   description,
-  shortTitle,
+  blogKeyword,
   title,
 }
