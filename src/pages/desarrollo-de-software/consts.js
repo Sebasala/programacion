11d757e5cdcc3390664ed625699f7a940e607a2b
Sebastián Salazar <ensuramerica@gmail.com>
2024-02-19 22:38:20 -0500

    Desarrollo de software hero

--- src/pages/desarrollo-de-software/consts.js
+++ src/pages/desarrollo-de-software/consts.js
@@ -1,11 +1,15 @@
+const alt = 'Computador';
+const blogKeyword = 'Desarrollo de Software';
 const blogPath = '/desarrollo-de-software';
 const description = 'Explora nuestro blog de desarrollo de software para obtener consejos, tutoriales y recursos que te ayudarán a dominar el arte del desarrollo de aplicaciones y programas.';
-const blogKeyword = 'Desarrollo de Software';
-const subtitle = 'Tips, Tutoriales y Tendencias | Shan.Lat';
+const image = 'computador.png'
+const subtitle = 'Tips, Tutoriales y Tendencias. Shan.Lat';
 
-export { 
+export {
+  alt,
   blogKeyword,
   blogPath,
   description,
+  image,
   subtitle,
 }
