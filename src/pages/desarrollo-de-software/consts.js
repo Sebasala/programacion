3e35024d0fdce5a7ab4fbb96704b8d5a21884cbd
Sebastián Salazar <ensuramerica@gmail.com>
2024-02-21 20:51:37 -0500

    Node JS card layout desktop

--- src/pages/desarrollo-de-software/consts.js
+++ src/pages/desarrollo-de-software/consts.js
@@ -2,7 +2,7 @@ const alt = 'Computador';
 const blogKeyword = 'Desarrollo de Software';
 const blogPath = '/desarrollo-de-software';
 const description = 'Explora nuestro blog de desarrollo de software para obtener consejos, tutoriales y recursos que te ayudarán a dominar el arte del desarrollo de aplicaciones y programas.';
-const image = 'computador.png'
+const image = '/computador.png'
 const subtitle = 'Tips, Tutoriales y Tendencias. Shan.Lat';
 
 export {
