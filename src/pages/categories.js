8eb34500fe69f5caf0887f8340abd853e34035b5
Sebastián Salazar <ensuramerica@gmail.com>
2024-02-26 21:42:38 -0500

    Feed marketing digital

---
+++ src/pages/categories.js
@@ -0,0 +1,20 @@
+const categories = {
+  "desarrollo-de-software": {
+    alt: 'Computador personal vintage de escritorio',
+    blogPath: '/desarrollo-de-software',
+    description: 'Explora nuestro blog de desarrollo de software para obtener consejos, tutoriales y recursos que te ayudarán a dominar el arte del desarrollo de aplicaciones y programas.',
+    image: '/computador.png',
+    subtitle: 'Tips, Tutoriales y Tendencias. Shan.Lat',
+    title: 'Desarrollo de Software',
+  },
+  "marketing-digital": {
+    alt: 'Megafono vintage',
+    blogPath: '/marketing-digital',
+    description: 'Descubre las últimas tendencias, estrategias efectivas y consejos prácticos en nuestro blog sobre marketing digital. Obtén insights sobre cómo mejorar tu presencia online, mejorar la fidelidad de tus clientes y alcanzar tus objetivos comerciales. ¡Explora ahora!',
+    image: '/megafono.png',
+    subtitle: 'Tendencias, Trucos y Herramientas. Shan.Lat',
+    title: 'Marketing Digital',
+  },
+}
+
+export default categories;
